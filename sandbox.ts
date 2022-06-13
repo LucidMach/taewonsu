a3379330fea5aaa6b1842116a352d7ca8ac93fb4
LucidMach <nukalasuraj7@gmail.com>
2022-06-13 11:41:38 +0530

    get top 3 daily animemes

---
+++ sandbox.ts
@@ -0,0 +1,27 @@
+// ToDo
+// [x] - get top 3 post in past 24hrs from r/animemes
+// [] - check if post is posted
+// [] - if not poseted then post @taewonsu
+// [] - add post to posted_list
+
+// https://www.reddit.com/r/Animemes/top.json?limit=3 - {url, id, title, author}
+
+import fs from "fs";
+import axios from "axios";
+import { join } from "path";
+
+interface animeme {
+  data: {
+    url: string;
+    id: string;
+    author: string;
+    title: string;
+  };
+}
+
+let raw_data = fs.readFileSync(join(".", "posted.json"));
+let posted_animemes = JSON.parse(raw_data.toString());
+
+axios
+  .get("https://www.reddit.com/r/Animemes/top.json?limit=3")
+  .then((res) => res.data.data.children);
